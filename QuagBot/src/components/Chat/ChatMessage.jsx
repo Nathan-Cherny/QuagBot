b49a1f95c820a054508a237fe16d11ecec897f04
Nathan Cherny <natec3632@gmail.com>
2026-09-21 11:25:28 -0400

    copy button

--- QuagBot/src/components/Chat/ChatMessage.jsx
+++ QuagBot/src/components/Chat/ChatMessage.jsx
@@ -1,4 +1,6 @@
 import UserAvatar from '../User/UserAvatar';
+import { Copy } from 'lucide-react';
+
 
 const BOT_AVATAR_SRC = '/QuagBotLogo.png';
 
@@ -9,11 +11,22 @@ function ChatMessage({ role, text }) {
     <div className={`chat-message ${isUser ? 'chat-message--user' : 'chat-message--assistant'}`}>
       {!isUser && <img src={BOT_AVATAR_SRC} alt="Bot" className="chat-avatar" />}
 
-      <div className="chat-message-bubble">{text}</div>
+      <div className="chat-message-bubble relative">
+        {text}
+        {!isUser && <CopyButton text={text} />}
+      </div>
 
       {isUser && <UserAvatar className="chat-avatar" />}
     </div>
   );
+}
+
+function CopyButton({text}) {
+  function copyText(text) { navigator.clipboard.writeText(text) }
+
+  return (
+    <Copy onClick={() => copyText(text)} className='absolute top-11 cursor-pointer hover:scale-110' size={16} />
+  )
 }
 
 export default ChatMessage;
